22d6256e62fa71ec95d38839c408901453374066
elykh1993 <120404324+elykh1993@users.noreply.github.com>
2024-11-24 11:59:14 +0330

    App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <Weather defaultCity="Sydney" />
+      <Weather defaultCity="Tehran" />
       <Author />
     </div>
   );
